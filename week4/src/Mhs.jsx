b34acf625d26a746f55a26c409580f5aab1c6a10
andikaa17 <andikaaprianto774@gmail.com>
2026-04-09 17:35:05 +0700

    Pemisahan File

---
+++ week4/src/Mhs.jsx
@@ -0,0 +1,20 @@
+import Nama from '../Nama.jsx';
+import Nim  from '../Nim.jsx';
+
+function Mhs(){
+     const list_mhs = [
+    {
+    nama : "Andika Apriyanto",
+    nim  : "A11.2023.15341"
+   },
+   {
+    nama : "Andika Apriyanto",
+    nim  : "A11.2023.15341"
+   },
+   {
+    nama : "Andika Apriyanto",
+    nim  : "A11.2023.15341"
+   }
+]
+};
+
