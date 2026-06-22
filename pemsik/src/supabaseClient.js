520803d73922c446996ed1014b72105ae9f0fa76
andikaa17 <andikaaprianto774@gmail.com>
2026-06-22 16:58:53 +0700

    update client

--- pemsik/src/supabaseClient.js
+++ pemsik/src/supabaseClient.js
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://maehsijminhyaofbswzx.supabase.co";
-const supabaseKey = "sb_publishable_Uk9SD2E6gxXZPMa18hLGDA_YIJRT_w7";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  console.error("Missing Supabase environment variables!");
+  throw new Error("Supabase URL and ANON Key are required");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
